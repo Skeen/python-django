8d2572e81dcdf6942034150f9f834a8016d5a028
Emil Madsen <sovende@gmail.com>
2019-09-14 17:17:56 +0200

    Fix change

--- django_opentracing/__init__.py
+++ django_opentracing/__init__.py
@@ -2,6 +2,5 @@ from .middleware import OpenTracingMiddleware  # noqa
 from .tracing import DjangoTracing  # noqa
 from .tracing import DjangoTracing as DjangoTracer  # noqa, deprecated
 from ._version import get_versions
-from .db import *
 __version__ = get_versions()['version']
 del get_versions

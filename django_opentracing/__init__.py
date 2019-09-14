e52acf3b2f50a9959caed3c8f01988ee4e132a65
Emil Madsen <sovende@gmail.com>
2019-09-14 17:23:31 +0200

    HELLO

--- django_opentracing/__init__.py
+++ django_opentracing/__init__.py
@@ -2,5 +2,6 @@ from .middleware import OpenTracingMiddleware  # noqa
 from .tracing import DjangoTracing  # noqa
 from .tracing import DjangoTracing as DjangoTracer  # noqa, deprecated
 from ._version import get_versions
+from .db import hello
 __version__ = get_versions()['version']
 del get_versions
